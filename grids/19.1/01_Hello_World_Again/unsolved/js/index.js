47a8b19be9396e785b91afb303fd44a8458a71c8
e18rojas <73149479+e18rojas@users.noreply.github.com>
2021-03-13 14:06:22 -0500

    19 activities

---
+++ grids/19.1/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,14 @@
+// Create a variable below
+
+
+var helloWorld = 'Hello this is Edita';
+
+
+
+// Console.log() your variable here.
+
+console.log(helloWorld)
+
+alert(helloWorld);
+
+alert("yes I'm here");
